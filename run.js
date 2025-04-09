aacb35c393f7adbe17e3e42931c94268c4cf459e
Kenny Allen <kenny.allen996@gmail.com>
2025-04-09 14:47:52 +0100

    stuff

---
+++ run.js
@@ -0,0 +1,11 @@
+let common = require('./common.js');
+
+(async ()=>{
+    // await require('./scripts/makeBins.js').run()
+    await require('./scripts/itemsRedo.js').run()
+    // await require('./scripts/BOMS.js').run()
+    // await require('./scripts/saleOrders2.js').run()
+    // await require('./scripts/manufacturing.js').run()
+    // await require('./scripts/POs.js').run()
+    // await require('./scripts/vouchers.js').run()
+})()
